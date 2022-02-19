ffd1f310fd5c894a7cbdb0b0af83187607fe8018
Florent CHAMFROY <florent.chamfroy@graviteesource.com>
2022-02-19 22:34:52 +0100

    style: setup & apply prettier

--- src/main/java/io/gravitee/fetcher/http/HttpFetcher.java
+++ src/main/java/io/gravitee/fetcher/http/HttpFetcher.java
@@ -31,15 +31,14 @@ import io.vertx.core.buffer.Buffer;
 import io.vertx.core.http.*;
 import io.vertx.core.net.ProxyOptions;
 import io.vertx.core.net.ProxyType;
+import java.io.ByteArrayInputStream;
+import java.net.URI;
+import java.util.concurrent.CompletableFuture;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.scheduling.support.CronSequenceGenerator;
-
-import java.io.ByteArrayInputStream;
-import java.net.URI;
-import java.util.concurrent.CompletableFuture;
 
 /**
  * @author David BRASSELY (david.brassely at graviteesource.com)
@@ -56,24 +55,31 @@ public class HttpFetcher implements Fetcher {
 
     @Value("${httpClient.timeout:10000}")
     private int httpClientTimeout;
+
     @Value("${httpClient.proxy.type:HTTP}")
     private String httpClientProxyType;
 
     @Value("${httpClient.proxy.http.host:#{systemProperties['http.proxyHost'] ?: 'localhost'}}")
     private String httpClientProxyHttpHost;
+
     @Value("${httpClient.proxy.http.port:#{systemProperties['http.proxyPort'] ?: 3128}}")
     private int httpClientProxyHttpPort;
+
     @Value("${httpClient.proxy.http.username:#{null}}")
     private String httpClientProxyHttpUsername;
+
     @Value("${httpClient.proxy.http.password:#{null}}")
     private String httpClientProxyHttpPassword;
 
     @Value("${httpClient.proxy.https.host:#{systemProperties['https.proxyHost'] ?: 'localhost'}}")
     private String httpClientProxyHttpsHost;
+
     @Value("${httpClient.proxy.https.port:#{systemProperties['https.proxyPort'] ?: 3128}}")
     private int httpClientProxyHttpsPort;
+
     @Value("${httpClient.proxy.https.username:#{null}}")
     private String httpClientProxyHttpsUsername;
+
     @Value("${httpClient.proxy.https.password:#{null}}")
     private String httpClientProxyHttpsPassword;
 
@@ -83,14 +89,16 @@ public class HttpFetcher implements Fetcher {
     @Autowired
     private Node node;
 
-
     public HttpFetcher(HttpFetcherConfiguration httpFetcherConfiguration) {
         this.httpFetcherConfiguration = httpFetcherConfiguration;
     }
 
     @Override
     public Resource fetch() throws FetcherException {
-        if (httpFetcherConfiguration.isAutoFetch() && (httpFetcherConfiguration.getFetchCron() == null || httpFetcherConfiguration.getFetchCron().isEmpty())) {
+        if (
+            httpFetcherConfiguration.isAutoFetch() &&
+            (httpFetcherConfiguration.getFetchCron() == null || httpFetcherConfiguration.getFetchCron().isEmpty())
+        ) {
             throw new FetcherException("Some required configuration attributes are missing.", null);
         }
 
@@ -127,12 +135,12 @@ public class HttpFetcher implements Fetcher {
         boolean ssl = HTTPS_SCHEME.equalsIgnoreCase(requestUri.getScheme());
 
         final HttpClientOptions options = new HttpClientOptions()
-                .setSsl(ssl)
-                .setTrustAll(true)
-                .setMaxPoolSize(1)
-                .setKeepAlive(false)
-                .setTcpKeepAlive(false)
-                .setConnectTimeout(httpClientTimeout);
+            .setSsl(ssl)
+            .setTrustAll(true)
+            .setMaxPoolSize(1)
+            .setKeepAlive(false)
+            .setTcpKeepAlive(false)
+            .setConnectTimeout(httpClientTimeout);
 
         if (httpFetcherConfiguration.isUseSystemProxy()) {
             ProxyOptions proxyOptions = new ProxyOptions();
@@ -153,24 +161,26 @@ public class HttpFetcher implements Fetcher {
 
         final HttpClient httpClient = vertx.createHttpClient(options);
 
-        final int port = requestUri.getPort() != -1 ? requestUri.getPort() :
-                (HTTPS_SCHEME.equals(requestUri.getScheme()) ? 443 : 80);
+        final int port = requestUri.getPort() != -1 ? requestUri.getPort() : (HTTPS_SCHEME.equals(requestUri.getScheme()) ? 443 : 80);
 
         try {
-            String relativeUri = (requestUri.getRawQuery() == null) ? requestUri.getRawPath() :
-                    requestUri.getRawPath() + '?' + requestUri.getRawQuery();
+            String relativeUri = (requestUri.getRawQuery() == null)
+                ? requestUri.getRawPath()
+                : requestUri.getRawPath() + '?' + requestUri.getRawQuery();
 
             final RequestOptions reqOptions = new RequestOptions()
-                    .setMethod(HttpMethod.GET)
-                    .setPort(port)
-                    .setHost(requestUri.getHost())
-                    .setURI(relativeUri)
-                    .putHeader(HttpHeaders.USER_AGENT, NodeUtils.userAgent(node))
-                    .putHeader("X-Gravitee-Request-Id", UUID.toString(UUID.random()))
-                    .setTimeout(httpClientTimeout);
+                .setMethod(HttpMethod.GET)
+                .setPort(port)
+                .setHost(requestUri.getHost())
+                .setURI(relativeUri)
+                .putHeader(HttpHeaders.USER_AGENT, NodeUtils.userAgent(node))
+                .putHeader("X-Gravitee-Request-Id", UUID.toString(UUID.random()))
+                .setTimeout(httpClientTimeout);
 
-            httpClient.request(reqOptions)
-                    .onFailure(new Handler<Throwable>() {
+            httpClient
+                .request(reqOptions)
+                .onFailure(
+                    new Handler<Throwable>() {
                         @Override
                         public void handle(Throwable throwable) {
                             promise.fail(throwable);
@@ -178,8 +188,10 @@ public class HttpFetcher implements Fetcher {
                             // Close client
                             httpClient.close();
                         }
-                    })
-                    .onSuccess(new Handler<HttpClientRequest>() {
+                    }
+                )
+                .onSuccess(
+                    new Handler<HttpClientRequest>() {
                         @Override
                         public void handle(HttpClientRequest request) {
                             request.response(asyncResponse -> {
@@ -219,7 +231,8 @@ public class HttpFetcher implements Fetcher {
 
                             request.end();
                         }
-                    });
+                    }
+                );
         } catch (Exception ex) {
             logger.error("Unable to fetch content using HTTP", ex);
             promise.fail(ex);
